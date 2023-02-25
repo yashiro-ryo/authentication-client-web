96ceb46abb63e73747b2213d01855e5ee059307b
yashiroryo <>
2023-02-25 13:39:00 +0900

    feat: signin, signupページのhtml構造を移行

--- src/components/pages/Signin.vue
+++ src/components/pages/Signin.vue
@@ -1,1 +1,46 @@
-<template>signin page</template>
+<template>
+  <div class="container">
+    <div class="signin-form">
+      <div class="app-name">
+        <p>タスク管理アプリ</p>
+      </div>
+      <div class="card">
+        <form id="form">
+          <div class="mb-3">
+            <label class="form-label">メールアドレス</label>
+            <input
+              type="email"
+              class="form-control"
+              aria-describedby="emailHelp"
+              placeholder="メールアドレス"
+              id="form-email"
+            />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">パスワード</label>
+            <input
+              type="password"
+              class="form-control"
+              placeholder="パスワード"
+              id="form-pass"
+            />
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" />
+            <label class="form-check-label" id="form-save-signin-info"
+              >ログイン情報を保存する</label
+            >
+          </div>
+        </form>
+        <p id="err-msg"></p>
+        <button id="submit-btn" class="btn btn-primary" onClick="submit()">
+          ログイン
+        </button>
+      </div>
+      <div class="link-group">
+        <a href="signup">アカウント新規作成</a>
+        <a href="forget">パスワードを忘れた場合はこちら</a>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/pages/Signup.vue
+++ src/components/pages/Signup.vue
@@ -1,1 +1,66 @@
-<template>signup page</template>
+<template>
+  <div class="container">
+    <div class="app-name">
+      <p>アカウント作成</p>
+    </div>
+    <div class="card">
+      <form id="form">
+        <div class="mb-3">
+          <label class="form-label">名前</label>
+          <input
+            type="text"
+            class="form-control"
+            id="form-name"
+            placeholder="名前"
+          />
+          <p class="error-text" id="name-error"></p>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">メールアドレス</label>
+          <input
+            type="email"
+            class="form-control"
+            id="form-email"
+            aria-describedby="emailHelp"
+            placeholder="メールアドレス"
+          />
+          <p class="error-text" id="email-error"></p>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">パスワード</label>
+          <input
+            type="password"
+            class="form-control"
+            id="form-pass"
+            placeholder="パスワード"
+          />
+          <div id="emailHelp" class="form-text">8文字以上の英数字</div>
+          <p class="error-text" id="pass-error"></p>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">パスワード確認</label>
+          <input
+            type="password"
+            class="form-control"
+            id="form-pass-confirm"
+            placeholder="パスワード"
+          />
+          <p class="error-text" id="pass-confirm-error"></p>
+        </div>
+      </form>
+      <p id="err-msg"></p>
+      <button
+        type="submit"
+        class="btn btn-primary"
+        id="submit-btn"
+        onclick="submit()"
+      >
+        登録
+      </button>
+    </div>
+    <div class="link-group">
+      <a href="/signin">ログインはこちら</a>
+      <a href="/forget">パスワードを忘れた場合はこちら</a>
+    </div>
+  </div>
+</template>
